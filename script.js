5d33089a3ee07e1a072177d68158ecd536a23bef
pankajjainn <jainpankaj270604@gmail.com>
2025-01-28 17:26:38 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+let api = "https://randomuser.me/api";
+
+async function fetchUser() {
+  try {
+    let fetchedData = await fetch(api);
+    // console.log(fetchedData);
+
+    let fetchedUser = await fetchedData.json();
+    // console.log(fetchedUser);
+
+    let users = fetchedUser.results[0];
+    // console.log(user);
+
+    localStorage.setItem("random-user", users);
+    JSON.stringify(users);
+
+    displayUser(users);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayUser(user) {
+  document.querySelector("#user-image").src = user.picture.large;
+  document.querySelector(
+    "#user-name"
+  ).innerHTML = `Name: ${user.name.title} ${user.name.first} ${user.name.last}`;
+
+  document.querySelector("#user-email").innerHTML = `Email: ${user.email}`;
+  document.querySelector("#user-gender").innerHTML = `Gender: ${user.gender}`;
+  document.querySelector(
+    "#user-address"
+  ).innerHTML = `Address: ${user.location.city}, ${user.location.state}, ${user.location.country}`;
+}
+
+// function pageLoading() {
+//   let storedUser = localStorage.getItem("random-user");
+//   if (storedUser) {
+//     let users = JSON.parse(storedUser);
+//     displayUser(users);
+//   } else {
+//     fetchUser();
+//   }
+// }
+
+let btn = document.querySelector("#new-user");
+
+btn.addEventListener("click", fetchUser);
+
+fetchUser()
